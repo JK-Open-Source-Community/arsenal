155e493e07f0c1f0b2da9a24bf1548e3d82e2710
Aditya Siddheshwar <46048299+aditya-siddheshwar@users.noreply.github.com>
2022-10-22 01:14:55 +0530

    constructor

---
+++ cpp/constructor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+struct Rectangle
+{
+    int width, height;
+    Rectangle(int w, int h)
+    {
+        width = w;
+        height = h;
+    }
+    void areaOfRectangle()
+    {
+        cout << "Area of Rectangle is: " << (width * height);
+    }
+};
+int main(void)
+{
+    struct Rectangle rec = Rectangle(4, 6);
+    rec.areaOfRectangle();
+    return 0;
+}
